214579e9b4ca48b0178509668006479eb6ef70a1
Simon Porter <simonporter007@users.noreply.github.com>
2024-05-16 23:31:31 +0100

    remove ec plugins cos of css bug

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,9 +1,6 @@
 import { defineEcConfig } from 'astro-expressive-code';
-import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
-import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
 
 export default defineEcConfig({
-  plugins: [pluginLineNumbers(), pluginCollapsibleSections()],
   emitExternalStylesheet: true,
   defaultProps: {
     wrap: true,
